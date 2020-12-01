91c152ee4a6ace023e306034930cb8b4c96ec6ff
Ev1lbl0w <ricasubtil@gmail.com>
2020-12-01 15:10:10 +0000

    Updated bootstrap code

--- bootstrap/script.py
+++ bootstrap/script.py
@@ -5,9 +5,20 @@ input_file = open('input', 'r')
 input_list = input_file.readlines()
 input_file.close()
 
-# Implement the algorithm
+answer_p1 = None
+answer_p2 = None
 
+# Implement the algorithm for part 1
+
+print("-------------\nFirst part...\n-------------\n")
+# TODO
+
+# Implement the algorithm for part 2
+
+print("\n--------------\nSecond part...\n--------------\n")
 # TODO
 
 # Final output
-print("")
+print('\n---------------')
+print(f'Final answer (part 1): "{answer_p1}"')
+print(f'Final answer (part 2): "{answer_p2}"')
