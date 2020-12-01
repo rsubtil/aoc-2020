180fdeab4b919c57ec28cc47fa399aa270c3838e
Ev1lbl0w <ricasubtil@gmail.com>
2020-12-01 14:43:56 +0000

    Created bootstrap code

---
+++ bootstrap/README.md
@@ -0,0 +1,11 @@
+# Day X: .......
+
+## Problem statement
+
+### Input
+
+### Output
+
+## Algorithm analysis
+
+### Time and memory complexity
---
+++ bootstrap/script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Get the input file
+input_file = open('input', 'r')
+input_list = input_file.readlines()
+input_file.close()
+
+# Implement the algorithm
+
+# TODO
+
+# Final output
+print("")
